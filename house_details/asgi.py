f73fa99d29d6b2b2feb9e38e813d6fe32cab8555
vamshi <vv264842>
2025-11-15 14:43:34 +0530

    system modifications

--- house_details/asgi.py
+++ house_details/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for revision project.
+ASGI config for house_details project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'revision.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'house_details.settings')
 
 application = get_asgi_application()
